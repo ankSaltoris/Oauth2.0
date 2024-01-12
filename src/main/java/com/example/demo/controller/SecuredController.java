183af3b698a5045db2f78c5601b060e35b2694a1
ankSaltoris <ankit.chaubey@saltoris.com>
2024-01-12 13:59:46 +0530

    first commit

--- src/main/java/com/example/demo/controller/SecuredController.java
+++ src/main/java/com/example/demo/controller/SecuredController.java
@@ -1,21 +1,16 @@
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
-@Component
-@ResponseBody
-
+@RestController
+@RequestMapping("/api/v1/controller")
 public class SecuredController {
 
-    @GetMapping("/home")
-    public String securedHome() {
-       
-        return "secured-home";
-    }
+  @GetMapping
+  public ResponseEntity<String> sayHello() {
+    return ResponseEntity.ok("Hello OAuth2");
+  }
 }
